d0ebbba5dcbd7b124d74df95ebc0c996e501825c
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-05 20:10:34 +0900

    qna

--- qna_add.php
+++ qna_add.php
@@ -1,47 +1,52 @@
 <?php
-    require "sql_connect.php";
-    session_start();
-    $title = $_POST['title'];
-    $author = $_SESSION['loginID'];
-    $content = $_POST['content'];
-    
-    if (empty($author)) {
-        //경고창 띄우기
-        echo
-            "<script>
+require "sql_connect.php";
+session_start();
+$title = $_POST['title'];
+$author = $_SESSION['loginID'];
+$content = $_POST['content'];
+
+if (empty($author)) {
+    //경고창 띄우기
+    echo
+        "<script>
             alert('로그인 후 이용하세요.');
             </script>";
-        //이전 페이지로 이동
-        echo "<script>history.back();</script>";
-    } elseif (empty(trim($title))) {
-        //경고창 띄우기
-        echo
-            "<script>
+    //이전 페이지로 이동
+    echo "<script>history.back();</script>";
+    exit;
+} elseif (empty(trim($title))) {
+    //경고창 띄우기
+    echo
+        "<script>
             alert('제목은 비우실 수 없습니다.');
             </script>";
-        //이전 페이지로 이동
-        echo "<script>history.back();</script>";
-    } elseif (empty(trim($content))) {
-        //경고창 띄우기
-        echo
-            "<script>
+    //이전 페이지로 이동
+    echo "<script>history.back();</script>";
+    exit;
+} elseif (empty(trim($content))) {
+    //경고창 띄우기
+    echo
+        "<script>
             alert('내용은 비우실 수 없습니다.');
             </script>";
-        //이전 페이지로 이동
-        echo "<script>history.back();</script>";
+    //이전 페이지로 이동
+    echo "<script>history.back();</script>";
+    exit;
+} else {
+    $sql_str = "insert into qna set
+        subject='$title',
+        user_id='$author',
+        content='$content'";
+    $return = sql_con($sql_str);
+    if ($return) {
+        echo "글 등록 성공";
+    } else {
+        echo "글 등록 실패";
     }
 
-    
-    $sql_str = "insert into qna set
-            subject='$title',
-            user_id='$author',
-            content='$content'";
-    $return = sql_con($sql_str);
-    if($return){
-        echo "글 등록 성공";
-    }
-    else {
-        echo "글 등록 실패";
-    }    
+}
+
+
+
 
 ?>
