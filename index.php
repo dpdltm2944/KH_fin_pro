66e08c424746c4ef72d98964c1845cc662eb8d47
KIMTERRY <bjsan213@gmail.com>
2023-09-05 17:15:20 +0900

    오류 수정

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
             while ($result = mysqli_fetch_array($return))
             {
                 ?>
-                <a href="/test_view.php?test_no=<?php echo $result['test_no'];?>번호 : <?php echo $result['test_no']; ?>  <?php echo $result['test_name']; ?>  <?php echo $result['test_date']; ?> <?php echo $result['test_place']; ?></a> <br>
+                <a href="/test_view.php?test_no=<?php echo $result['test_no'];?>">번호 : <?php echo $result['test_no']; ?>  <?php echo $result['test_name']; ?>  <?php echo $result['test_date']; ?> <?php echo $result['test_place']; ?></a> <br>
                 <?php    
             }     
             ?>
