edfaffa35b9ad116d8645b0f1c747a96b3c97536
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-05 15:27:33 +0900

    qna - list

--- qna.php
+++ qna.php
@@ -49,7 +49,8 @@
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
                     echo '<li>
-                            <strong>' . $row['subject'] . ':</strong> ' . $row['user_id'] . '
+                            <strong>' . $row['subject'] . ':</strong> 
+                            <p>' . $row['user_id'] . '</p>
                             <p>' . $row['content'] . '</p>
                           </li>';
                 }
