f24b2cfb9fbf28b31c2602387b1ee0ec2bf1a987
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-06 09:34:20 +0900

    qna

--- profile.php
+++ profile.php
@@ -48,18 +48,17 @@
         <div class="profile-info">
             <?php
             if($result['img_path'] ==  NULL || $result['img_path'] == "" || $result['img_path'] == "./img/profile.png"){
-                echo "<img src=\"./img/profile.png\" alt=\"프로필 이미지\">";
-                echo "<br>";
-            }
-            else{
-                echo "<img src=\"".$result['img_path']."\" alt=\"프로필 이미지\">";
-            }
-                
                 //프로필 사진 등록 버튼
                 echo "<form action=\"profile_img.php\" method=\"post\" enctype=\"multipart/form-data\">";
                 echo "<input type=\"file\" name=\"img\" id=\"img\">";
                 echo "<input type=\"submit\" value=\"프로필 사진 등록\" name=\"submit\">";
                 echo "</form>";
+            }
+            else{
+                echo "<img src=\"".$result['img_path']."\" alt=\"프로필 이미지\">";
+            }
+               
+                
             ?>
             <img src="<?php ?>" alt="프로필 이미지">
 
