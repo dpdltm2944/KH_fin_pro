4e00ff53606ea58c4e0bda57dec26ddf16cac3fe
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-06 09:11:06 +0900

    profile_add

--- profile.php
+++ profile.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ko">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>프로필</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <nav>
@@ -15,11 +17,11 @@
                 <li><a href="/notice.php">공지사항</a></li>
                 <li><a href="/qna.php">문의하기</a></li>
                 <li><a href="/profile.php">마이페이지</a></li>
-                <?php 
+                <?php
                 session_start();
-                if($_SESSION['loginID']== ""){
-                echo "<li><a href=\"/login.php\">로그인</a></li>";
-                }else{
+                if ($_SESSION['loginID'] == "") {
+                    echo "<li><a href=\"/login.php\">로그인</a></li>";
+                } else {
                     echo "<li><a href=\"/logout.php\">로그아웃</a></li>";
                 }
                 ?>
@@ -27,33 +29,57 @@
         </nav>
     </header>
 
+
     <section class="section profile">
+        <?php
+        if ($_SESSION['loginID']) {
+            $id = $_SESSION['loginID'];
+
+            require "sql_connect.php";
+
+            $sql_str = "SELECT name , age , sex , address, img_path FROM user WHERE id='$id'";
+            $return = sql_con($sql_str);
+        } else {
+            echo "<script>alert('로그인 후 이용하세요.');history.back();</script>";
+        }
+        $result = mysqli_fetch_array($return);
+        ?>
         <h1>나의 프로필</h1>
         <div class="profile-info">
-            <img src="/img/11.jpg" alt="프로필 이미지">
-            
-            
-            
-           
-
             <?php
-            if ($_SESSION['loginID']) {
-                $id = $_SESSION['loginID'];
-        
-                require "sql_connect.php";
-        
-                $sql_str = "SELECT name , age , sex , address FROM user WHERE id='$id'";
-                $return = sql_con($sql_str);
-            }else{
-                echo "<script>alert('로그인 후 이용하세요.');history.back();</script>";
+            if ($result['img_path'] == null) {
+                echo "<img src=\"./img/profile.png\" alt=\"프로필 이미지\">";
+                echo "<br>";
+                //프로필 사진 등록 버튼
+                echo "<form action=\"profile_img.php\" method=\"post\" enctype=\"multipart/form-data\">";
+                echo "<input type=\"file\" name=\"fileToUpload\" id=\"fileToUpload\">";
+                echo "<input type=\"submit\" value=\"프로필 사진 등록\" name=\"submit\">";
+                echo "</form>";
+
+
             }
-            $result = mysqli_fetch_array($return);
             ?>
-            <h2>사용자 이름: <?php echo $result['name']; ?></h2>
-            <p>나이: <?php echo $result['age']; ?> </p>
-            <p>성별: <?php echo $result['sex']; ?></p>
-            <p>주소: <?php echo $result['address']; ?></p>
-            
+            <img src="<?php ?>" alt="프로필 이미지">
+
+
+
+
+
+
+
+            <h2>사용자 이름:
+                <?php echo $result['name']; ?>
+            </h2>
+            <p>나이:
+                <?php echo $result['age']; ?>
+            </p>
+            <p>성별:
+                <?php echo $result['sex']; ?>
+            </p>
+            <p>주소:
+                <?php echo $result['address']; ?>
+            </p>
+
         </div>
     </section>
 
@@ -63,4 +89,5 @@
         <p>&copy; 2023 </p>
     </footer>
 </body>
+
 </html>
---
+++ profile_img.php
@@ -0,0 +1,46 @@
+//프로필 사진 등록 php 코드
+
+// path: /img/profile_[user_id].php
+
+//
+// db에 파일 저장 결로/파일명 저장
+
+<?php
+require "sql_connect.php";
+session_start();
+$user_id = $_SESSION['loginID'];
+$img = $_FILES['img'];
+$filename = $img['name'];
+$filetype = $img['type'];
+$filesize = $img['size'];
+$tmp_name = $img['tmp_name'];
+$error = $img['error'];
+
+$upload_dir = "./img/";
+$upload_file = $upload_dir . basename($filename);
+// 파일 이름 proifle_[user_id].jpg 로 변경
+$upload_file = $upload_dir . "profile_" . $user_id . ".jpg";
+
+
+//파일 업로드
+
+if ($error == 0) {
+    if (move_uploaded_file($tmp_name, $upload_file)) {
+        echo "파일 업로드 성공";
+        //db에 파일 저장 경로/파일명 저장
+        $sql_str = "update user set img_path='$upload_file' where id='$user_id'";
+        $return = sql_con($sql_str);
+        if ($return) {
+            echo "db에 파일 저장 성공";
+        } else {
+            echo "db에 파일 저장 실패";
+        }
+    } else {
+        echo "파일 업로드 실패";
+    }
+} else {
+    echo "파일 업로드 실패";
+}
+
+
+
