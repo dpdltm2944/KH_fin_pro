eccec952bad8cfeac29c198d2f0fa9e21487061c
dpdltm2944 <dpdltm2944@gmail.com>
2023-09-06 19:58:36 +0900

    qna

--- test_proc.php
+++ test_proc.php
@@ -4,14 +4,20 @@
     //파라미터 저장
     $no=$_GET['test_no'];
     $user_no = $_SESSION['user_no'];
-    $user_name = $_SESSION['user_name'];
+    $user_id = $_SESSION['user_id'];
     $order_id = date('YmHis')+$no+$user_no;
-    //DB쿼리
+
     
+    $sql_str1 = "select user_name from user where user_no='$user_no'";
+    $return1 = sql_con($sql_str1);
+    //DB쿼리
     $sql_str = "insert into test_order set test_no='$no', user_no='$user_no', order_id='$order_id'";
-    $sql_str2 = "insert into test_result set test_no='$no', user_name='$user_name', order_id='$order_id'";
-    //쿼리 결과
     $return = sql_con($sql_str);
+    
+   
+    $sql_str2 = "insert into test_result set test_no='$no', user_name='$return1', order_id='$order_id'";
+    //쿼리 결과
+   
     $return2 = sql_con($sql_str2);
 
     if($return && $return2){
