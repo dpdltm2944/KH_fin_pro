4099c2dd999162d241bd3c234646481b1bc8ee8b
KIMTERRY <bjsan213@gmail.com>
2023-09-18 09:21:06 +0900

    오류 수정

--- filter.php
+++ filter.php
@@ -1,9 +1,9 @@
 <?php
      function web_filter($str){
-        $str = $str.str_replace("<","&lt;");
-        $str = $str.str_replace(">","&gt;");
-        $str = $str.str_replace("&","&amp;");
-        $str = $str.str_replace("\"","&quot;");
+        $str = $str.str_replace("<","&lt;",$str);
+        $str = $str.str_replace(">","&gt;",$str);
+        $str = $str.str_replace("&","&amp;",$str);
+        $str = $str.str_replace("\"","&quot;",$str);
         return $str;
     }
 
