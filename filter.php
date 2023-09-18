7b1de09aeb490554911a87584cffba53c61830e6
KIMTERRY <bjsan213@gmail.com>
2023-09-18 09:42:58 +0900

    test

--- filter.php
+++ filter.php
@@ -1,11 +1,6 @@
 <?php
      function web_filter($str){
-        $str.str_replace("<","&lt;",$str);
-        $str.str_replace(">","&gt;",$str);
-        $str.str_replace("&","&amp;",$str);
-        $str.str_replace("\"","&quot;",$str);
-        return $str;
+        return htmlspecialchars($str);
     }
 
-    
 ?>
